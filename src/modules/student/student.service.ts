6ad7927969249db1115a28d13d5ea18fdfbddd43
Likhon22 <likhonsarker793@gmail.com>
2024-11-29 18:20:09 +0600

    refactoring in error middlewares

--- src/modules/student/student.service.ts
+++ src/modules/student/student.service.ts
@@ -26,50 +26,6 @@ const getStudentsFromDB = async (query: Record<string, unknown>) => {
     .fields();
   const result = await searchQuery.modelQuery;
   return result;
-
-  // const search = StudentModel.find({
-  //   $or: studentSearchableField.map(field => ({
-  //     [field]: {
-  //       $regex: searchTerm,
-  //       $options: 'i',
-  //     },
-  //   })),
-  // });
-  // const queryObj = {
-  //   ...query,
-  // };
-  // const excludeFields = ['searchTerm', 'sort', 'page', 'limit', 'fields'];
-  // excludeFields.forEach(el => delete queryObj[el]);
-  // console.log({ query }, { queryObj });
-
-  // const filter = search.find(queryObj);
-  // const sort = query?.sort || '-createdAt';
-  // const sortQuery = filter.sort(sort as string);
-  // const page = Number(query?.page) || 1;
-  // const limit = Number(query?.limit) || 10;
-  // const skip = (page - 1) * limit;
-  // const paginateQuery = sortQuery.skip(skip).limit(limit);
-  // const fields = query?.fields
-  //   ? (query.fields as string).split(',').join(' ')
-  //   : '';
-  // const fieldsQuery = await paginateQuery.select(fields);
-  // return fieldsQuery;
-  // const studentQuery = new QueryBuilder(StudentModel.find(), query)
-  //   .search(studentSearchableField)
-  //   .filter()
-  //   .sort()
-  //   .paginate()
-  //   .fields();
-  // const result = await studentQuery.modelQuery
-  //   .populate('admissionSemester')
-  //   .populate({
-  //     path: 'academicDepartment',
-  //     populate: {
-  //       path: 'academicFaculty',
-  //     },
-  //   });
-
-  // return result;
 };
 
 const getSingleStudentFromDB = async (id: string) => {
@@ -150,8 +106,8 @@ const deleteStudentFromDB = async (id: string) => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (err) {
     await session.abortTransaction();
-    session.endSession();
-    throw new Error('Failed to delete student');
+    await session.endSession();
+    throw new AppError(400, 'Failed to delete user');
   }
 };
 
